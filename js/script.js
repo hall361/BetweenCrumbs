daf01f75801e73a70889ba53a5e05f329d9a112b
HECTOR ANIBAL LOPEZ LOPEZ <hall361@hotmail.com>
2024-09-22 17:55:21 -0500

    Preparación proyecto

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Agrega un evento de clic al botón de inicio de sesión
+document.getElementById("login-button").addEventListener("click", function() {
+    // Muestra un mensaje de bienvenida
+    alert("Bienvenido a BetweenCrumbs!");
+  });
